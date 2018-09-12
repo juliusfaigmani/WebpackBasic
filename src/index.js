08fec9eb44e33f1ec20130244116d9a8c2c2d9b4
nutframe <37786929+nutframe@users.noreply.github.com>
2018-09-12 15:14:04 +0800

    Initial

---
+++ src/index.js
@@ -0,0 +1,3 @@
+import notify from './Notification';
+
+notify('Hello World!');
---
+++ webpack.config.js
@@ -0,0 +1,17 @@
+var webpack = require('webpack');
+
+//var path = require('path');
+
+module.exports = {
+
+	'entry': './src/index.js',
+
+	'output':{
+
+		path: __dirname +'/dist',
+
+		filename: 'bundle.js'
+
+	}
+
+}
